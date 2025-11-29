c3be67be5629b813709f623057fb0379e5b2b80f
Jenny Kim <jennyykim1016@gmail.com>
2025-11-29 23:00:52 +0900

    Add files via upload

---
+++ env_setting.py
@@ -0,0 +1,9 @@
+env_content = """
+LLM_API_KEY="TODO"
+LLM_API_ENDPOINT="TODO"
+LLM_API_VERSION="TODO"
+"""
+
+env_content_dict = {'LLM_API_KEY': 'TODO',
+               'LLM_API_ENDPOINT': 'TODO',
+               'LLM_API_VERSION': 'TODO'}
